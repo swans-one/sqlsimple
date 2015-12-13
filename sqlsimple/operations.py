bb96f599a1fa3402923d923ab092c9f5440a6dc1
Erik Swanson <theerikswanson@gmail.com>
2015-12-12 20:02:13 -0500

    More operations

--- sqlsimple/operations.py
+++ sqlsimple/operations.py
@@ -1,8 +1,9 @@
+# TODO: Accept parameters! In all of these functions
 from sqlsimple.utils import run_sql_query, run_sql_exec
 
 
 def sql_query(query, db='default'):
-    pass
+    return run_sql_query(sql, db)
 
 
 def sql_query_file(query_name=None, filename=None, db='default'):
@@ -11,6 +12,10 @@ def sql_query_file(query_name=None, filename=None, db='default'):
 
 def sql_exec(sql, db='default'):
     return run_sql_exec(sql, db)
+
+
+def sql_exec_many(sql, db='default'):
+    pass
 
 
 def sql_exec_file(exec_name=None, filename=None, db='default'):
