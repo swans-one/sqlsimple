7cc5f7a89e12f8fd5ba84e33418f584a01438997
Erik Swanson <theerikswanson@gmail.com>
2015-12-12 19:25:15 -0500

    Utils refactor, module definition

---
+++ sqlsimple/__init__.py
@@ -0,0 +1,2 @@
+from operations import sql_query, sql_query_file, sql_exec, sql_exec_file
+from utils import get_cursor
--- sqlsimple/utils.py
+++ sqlsimple/utils.py
@@ -1,24 +1,33 @@
+from collections import namedtuple
 from contextlib import contextmanager
 
 from sqlsimple.configuration import CONFIGURATION
 
 
 def run_sql_query(sql, db):
-    db_config = CONFIGURATION['databases'][db]
-    with get_cursor(db_config) as c:
-        results = c.fetchall(sql)
+    """Execute and return results for a query.
+    """
+    with get_cursor(db_config) as cur:
+        cur.execute(sql)
+        cols = [col[0] for col in cur.description]
+        Result = namedtuple('Result', cols)
+        results = map(Result._make, cur.fetchall())
     return results
 
 
 def run_sql_exec(sql, db):
-    db_config = CONFIGURATION['databases'][db]
+    """Execute a sql statement, but do not return results from a query.
+    """
     with get_cursor(db_config) as c:
         results = c.execute(sql)
     return results
 
 
 @contextmanager
-def get_cursor(db_config):
+def get_cursor(db='default'):
+    """Get a cursor object for a configured database, handle commit/rollback.
+    """
+    db_config = CONFIGURATION['databases'][db]
     # TODO: Think about caching this connection and just using context
     # managers differently.
     possible_connection_params = [
@@ -41,3 +50,11 @@ def get_cursor(db_config):
     finally:
         cursor.close()
         connection.close()
+
+
+def file_contents(exec_name=None, filename=None):
+    """Get the sql query contained in a file.
+
+    Search in sql directories to find sql files and cache the results.
+    """
+    pass
