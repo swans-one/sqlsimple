2c2b13672a9f77c5e3b4b913b7e5edbf7c93ebd4
Erik Swanson <theerikswanson@gmail.com>
2015-12-12 20:31:59 -0500

    Some configuration templates

---
+++ sqlsimple/templates/schema.sql
@@ -0,0 +1,8 @@
+-- Put your schema here. This is the exact schema that will be created
+-- when you run `sqlsimple init-db`. 
+--
+-- When making changes to this schema, it is up to you to create a
+-- corresponding migration. The reverse is also true.
+--
+-- It is also your job to keep any fixtures up to date with this
+-- schema.
